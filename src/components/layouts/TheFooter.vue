a9f8f1423afb6c300cdf40c36a4549255b405574
xingyanan <yygxyn@163.com>
2019-10-16 09:25:29 +0800

    vue header packaging components

---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>11</div>
+</template>
+
+<script>
+  export default {
+    name: "TheFooter.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav()">
+          <span class="sr-only">Toggle nav</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+      <div :class="['collapse', 'navbar-collapse', { in:showCollapsedNav }]" id="top-navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :key="item.id" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheHeader.vue",
+    data() {
+      return {
+        logo: {
+          src: `${this.uploadsUrl}`,
+          title: 'Vuejs'
+        },
+        navList: ['社区','问答','教程','头条'],
+        activeNavIndex: 0,
+        showCollapsedNav: false
+      }
+    },
+    beforeCreate() {
+      this.uploadsUrl = ''
+    },
+    methods: {
+      changeNavIndex(index){
+        this.activeNavIndex = index
+      },
+      toggleNav() {
+        this.showCollapsedNav = !this.showCollapsedNav
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .title {display:none;}
+    .navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+</style>
